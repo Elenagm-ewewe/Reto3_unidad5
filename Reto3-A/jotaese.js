2d327d487a461484075f0f22c1174a13d779f75a
JavviFdeez <javifdez195@gmail.com>
2025-12-01 19:12:52 +0100

    DESAROLLO RETO 3
    * Correccion y funcinando Reto 3A
    * Reto 3B Creado pero aun sin funcionar

--- Reto3-A/jotaese.js
+++ Reto3-A/jotaese.js
@@ -1,35 +1,40 @@
 let personaje = null;
 
+document.getElementById("formulario").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let error = document.getElementById("error");
+  let iden = document.getElementById("iden");
 
-document.getElementById("formulario").addEventListener("submit", function(e){
-e.preventDefault();
-let error = document.getElementById("error");
-let iden = document.getElementById("iden");
-
-
-if(iden.value === ""){
+  if (iden.value === "") {
     error.innerHTML = "Campo vacío";
-}else if(!iden.checkValidity()){
+  } else if (!iden.checkValidity()) {
     error.innerHTML = "Formato incorrrecto";
-}else{
-
+  } else {
     obtenerPersonaje(iden.value);
-    console.log(personaje)
-    document.getElementById("tarjeta").innerHTML = `<div class="tarjeta><strong>Nombre</strong><span>${personaje.result.properties}</span></div>`
-}
-
-
+  }
 });
 
-async function obtenerPersonaje(id){
-
-try{
-        const respuesta = await fetch(`https://www.swapi.tech/api/people/${id}/`)
-        personaje = await respuesta.json();
-        console.log("Hola desde la funcion")
-
-}catch{
-        document.getElementById("noencontrado").textContent = "El personaje no fue encontrado";
+async function obtenerPersonaje(id) {
+    const respuesta = await fetch(`https://www.swapi.tech/api/people/${id}/`);
+    personaje = await respuesta.json();
+  try {
+    if (personaje.result) {
+      const tarjeta = document.getElementById("tarjeta");
+      const personajeDiv = document.createElement("div");
+      personajeDiv.id = "personaje";
+      tarjeta.innerHTML = `<div class="tarjeta"><strong>Nombre: </strong><span>${personaje.result.properties.name}</span></div>
+        <div class="tarjeta"><strong>Altura: </strong><span>${personaje.result.properties.height}</span></div>
+        <div class="tarjeta"><strong>Peso: </strong><span>${personaje.result.properties.mass}</span></div>
+        <div class="tarjeta"><strong>Genero: </strong><span>${personaje.result.properties.gender}</span></div>
+        `;
+      document.body.appendChild(personajeDiv);
+      personajeDiv.appendChild(tarjeta);
+    } else {
+      document.getElementById("noencontrado").textContent =
+        "El personaje no fue encontrado";
     }
-    
+  } catch {
+    document.getElementById("noencontrado").textContent =
+      "Respuestas no exitosas de la API o problemas de conexión.";
+  }
 }
